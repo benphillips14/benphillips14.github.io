19337a7a39728b734c4c666c543e68cd4d0d69b1
Ben Phillips <benphillips@Bens-MacBook-Air.local>
2022-12-01 16:21:26 -0500

    css done

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
-// ref
+//variables for hole and obstacle elements
 
-var block = document.getElementById("obstacle");
-
+var obstacle = document.getElementById("obstacle");
 var hole = document.getElementById("hole");
 
-var character = document.getElementById("character");
-
-var jumping = 0;
-
-
-// using listner 
 hole.addEventListener('animationiteration', () => {
-    var random = -((Math.random()*300)+150);
-    hole.style.top = random + "px";
-    counter++;
